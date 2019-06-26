fd6acaa37cdb4c0f81c9c2fd2e2c0e40eda80530
github username <github email address>
2019-06-25 22:03:53 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -42,16 +42,16 @@ class Song
         genre_count
       end
 
-      def self.artist_count
-        artist_count = {}
+      def self.artist_hash
+        artist_hash = {}
         @@artists.each do |artist| 
-          if artist_count[artist]
-          artist_count[artist] += 1 
+          if artist_hash[artist]
+          artist_hash[artist] += 1 
           else
-          artist_count[artist] = 1
+          artist_hash[artist] = 1
           end
         end
-        artist_count
+        artist_hash
       end
 
 
